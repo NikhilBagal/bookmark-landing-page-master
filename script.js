adf02bbe2c0f1867fe57166b593635cd32786c37
nikhil bagal <nbagal02@gmail.com>
2019-10-02 14:35:50 +0530

    js code completed for tabs

--- script.js
+++ script.js
@@ -1,27 +1,30 @@
 window.onload = init;
 function init(){
-  // 
-   //;
   attr();
 }
 function attr(){
     var element = document.querySelectorAll('.tabs .tab-links a');
-  /*  var currentAttrValue = element.getAttribute('href');
-    console.log(currentAttrValue);*/
-    for(var i=0;i<element.length;i++){
-        element[i].addEventListener('click', function(e){
-            e.preventDefault();
-            var currentAttrValue = this.getAttribute('href');
-           // console.log(currentAttrValue);
-           var ele = document.querySelector('.tabs ' + currentAttrValue);
-          /* var previous = ele.previousSibling;
-           var next = ele.nextSibling;
-           console.log(previous);
-           console.log(next);
-           ele.classList.add('active');*/
-           console.log(ele.previousSibling);
 
-         //  element[i].parentElement.classList.add('active');
+    for(var i=0;i<element.length;i++){
+      /* binding event listeners to links */
+        element[i].addEventListener('click', function(e){
+          e.preventDefault();
+          /* removing active class from links */
+          var listElement = document.querySelector('.tabs .tab-links li');
+          var listChilds = listElement.parentNode.children;
+          for(var i=0;i<listChilds.length;i++){
+            listChilds[i].classList.remove('active');
+          }
+          /* adding active class to only one which clicked */
+          this.parentNode.classList.add('active');
+          /* displaying tabs */
+          var currentAttrValue = this.getAttribute('href');
+          var ele = document.querySelector('.tabs ' + currentAttrValue);
+          var childs = ele.parentNode.children;
+          for(var i=0;i<childs.length;i++){
+            childs[i].classList.remove('active');
+          }
+          ele.classList.add('active');
         })
         
     }
